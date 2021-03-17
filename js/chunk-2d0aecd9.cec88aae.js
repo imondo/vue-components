6cd81fe706d1fcdf546390b2e18b219280f031f5
one-pupil <one-pupil@users.noreply.github.com>
2021-03-17 02:01:55 +0000

    deploy: 119a42c0402b417bccf173c5f22cec37e4c4d966

---
+++ js/chunk-2d0aecd9.cec88aae.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0aecd9"],{"0c2b":function(t,n,e){"use strict";e.r(n);var r=e("7a23");function a(t,n,e,a,c,u){var o=Object(r["B"])("ani-stars");return Object(r["s"])(),Object(r["e"])(o,{"max-num":7,num:2,size:35,color:"#f1c40f",onGetStarNum:a.getStarNum},null,8,["onGetStarNum"])}var c={setup:function(){var t=Object(r["j"])(),n=t.proxy,e=function(t){n.$message("你提交了：".concat(t," 分！"))};return{getStarNum:e}}};c.render=a;n["default"]=c}}]);
+//# sourceMappingURL=chunk-2d0aecd9.cec88aae.js.map
